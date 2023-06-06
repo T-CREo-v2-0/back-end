82fae3c182a0e1afc11dcb684912648edbcb947d
Daniela Ramírez <16-10940@usb.ve>
2023-06-06 16:17:34 -0400

    db connected

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,19 @@
+import 'dotenv/config'
 import app from './app'
 import config from './config'
+import mongoose, { ConnectOptions } from 'mongoose'
+
+if (!process.env.DB_URI) {
+  throw new Error('DB_URI must be defined');
+}
+
+const uri = process.env.DB_URI as string;
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true} as ConnectOptions)
+  .then(() => console.log('DB Connected!'))
+  .catch( (err) => console.error(err))
 
 app.listen(config.PORT, () => {
   console.log('Server listening at port ' + config.PORT)
